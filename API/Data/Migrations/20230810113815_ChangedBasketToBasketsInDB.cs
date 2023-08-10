39b0b967a8a0c0f714552d78d0f68e21e5e665dc
OmarMohamed256 <omarmohamed2342@outlook.com>
2023-08-10 15:39:54 +0300

    added basket backend

---
+++ API/Data/Migrations/20230810113815_ChangedBasketToBasketsInDB.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedBasketToBasketsInDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketItems_Basket_BasketId",
+                table: "BasketItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Basket",
+                table: "Basket");
+
+            migrationBuilder.RenameTable(
+                name: "Basket",
+                newName: "Baskets");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Baskets",
+                table: "Baskets",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketItems_Baskets_BasketId",
+                table: "BasketItems",
+                column: "BasketId",
+                principalTable: "Baskets",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BasketItems_Baskets_BasketId",
+                table: "BasketItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Baskets",
+                table: "Baskets");
+
+            migrationBuilder.RenameTable(
+                name: "Baskets",
+                newName: "Basket");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Basket",
+                table: "Basket",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BasketItems_Basket_BasketId",
+                table: "BasketItems",
+                column: "BasketId",
+                principalTable: "Basket",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,10 +1,22 @@
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { Product } from "../../app/models/product";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+import { LoadingButton } from "@mui/lab";
 interface Props {
     product: Product;
 }
 export default function ProductCard({ product }: Props) {
+    const [loading, setLoading] = useState(false);
+
+    function handleAddItem(productId: number) {
+        setLoading(true);
+        agent.Basket.addItem(productId)
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }
+
     return (
         <Card>
             <CardHeader
@@ -32,8 +44,8 @@ export default function ProductCard({ product }: Props) {
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Add to cart</Button>
-                <Button component={Link} to={`/catalog/${product.id}`}  size="small">View</Button>
+                <LoadingButton loading={loading} onClick={() => handleAddItem(product.id)} size="small">Add to cart</LoadingButton loading={loading}>
+                <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
             </CardActions>
         </Card>
     )
